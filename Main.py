622db97688c0425c1ec18ac481601fa5281d1097
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2022-04-17 19:15:16 -0300

    Haciendo una version de como calcular el mu de maquina

--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
+import numpy
+
+def comparacion_simple(valor1 : float, valor2 : float) -> bool :
+    return numpy.float32(valor1) == numpy.float32(valor2)
+
+def comparacion_doble(valor1 : float, valor2 : float) -> bool :
+    return numpy.float64(valor1) == numpy.float64(valor2)
+
+def unidad_de_maquina(base : int, funcion_comparacion) -> int :
+    valor = 1
+    unidad = 0
+    while not funcion_comparacion(valor, valor/10):
+        valor /= base
+        unidad += 1
+    return unidad
 
 def main():
-    print("Hola")
+    unidad = unidad_de_maquina(10, comparacion_doble)
+    print(unidad)
 
 if __name__ == "__main__":
     main()
