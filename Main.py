f91b8180158b1fe4d7cd2eec5df4c3254ed9defd
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2022-04-20 14:33:08 -0300

    Poniendo lindo el main

--- Main.py
+++ Main.py
@@ -2,6 +2,12 @@ from UnidadDeMaquina import UnidadDeMaquina, ComparacionSimple, ComparacionDoble
 from Serie import FuncionSerie
 import numpy
 
+padron1 = 105859
+padron2 = 106005
+divisor = 10 ** 6
+
+valorPrueba = (padron1 + padron2) / divisor
+
 def UnidadDeMaquicaSegunBases(bases):
     for base in bases:
         print(f"Con la base {base}, tenemos las unidades de maquina: ")
@@ -11,19 +17,16 @@ def UnidadDeMaquicaSegunBases(bases):
 
         print(f"\tSimple: {unidadSimple} \n\tDoble: {unidadDoble}\n")
 
+def ResultadosSegunPrecisiones(valor : float, iteracion : int, precisiones):
+    for precision in precisiones:
+        print(f"Con una cantidad de iteraciones {iteracion}\n\tSe logra la precision: ", end = "")
+        resultado = FuncionSerie(valor, iteracion, precision)
+        print("{0:.55f}\n".format(numpy.float64(resultado)))
+
 def Main():
     UnidadDeMaquicaSegunBases([2, 10])
-
-    valorPrueba = (106005 + 105859) / (10 ** 6)
-    resultados = []
-
-    for iteracion in range(20):
-        resultado = FuncionSerie(valorPrueba, iteracion, numpy.float32)
-        resultados.append(resultado)
-    
-    for resultado in resultados:
-        print("{0:.32f}".format(numpy.float64(resultado)))
-    
+    iteracionesNecesarias = 10 # hacer el calculo de iteraciones
+    ResultadosSegunPrecisiones(valorPrueba, iteracionesNecesarias, [numpy.float32, numpy.float64])    
 
 if __name__ == "__main__":
     Main()
