f1d18a135e00c5e1a452360943c543edd727db98
Nico <nfacciano@fi.uba.ar>
2022-04-20 18:40:26 -0300

    Mostrando bien el cp y el te

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import numpy
 valorPrueba = (105859 + 106005) / 10 ** 6
 errorMinimo = 10 ** (-14)
 precisionesUsadas = [numpy.float32, numpy.float64]
+basesUsadas = [2, 10]
 
 def UnidadDeMaquicaSegunBases(bases, precisiones):
     for base in bases:
@@ -23,15 +24,25 @@ def ResultadosSegunPrecisiones(valor : float, iteracion : int, precisiones):
         resultado = FuncionSerie(valor, iteracion, precision)
         print("{0:.55f}\n".format(numpy.float64(resultado)))
 
+def CalculoDeCPYTEPorBases(valor : float, iteracion : int, presicionMayor, presicionMenor, bases):
+    for precision in [presicionMayor, presicionMenor]:
+        print(f"Con la precision {precision.__name__}:")        
+
+        cp = CaclularCPExperimental(FuncionSerie, valor, iteracion, precision)
+        print(f"\tLa Condicion del problema: {cp}")
+    
+    for base in bases:
+        print(f"Con la base {base}: ")
+        te = CalcularTEExperimental(FuncionSerie, valor, iteracion, presicionMayor, presicionMenor, base)
+        print(f"\tEl termino de estabilidad: {te}")
+
+
 def Main():
     valor = valorPrueba * 15
-    UnidadDeMaquicaSegunBases([2, 10], precisionesUsadas)
+    UnidadDeMaquicaSegunBases(basesUsadas, precisionesUsadas)
     iteracionesNecesarias = CantidadIteraciones(valor, errorMinimo)
     ResultadosSegunPrecisiones(valor, iteracionesNecesarias, precisionesUsadas)
-
-    cp = CaclularCPExperimental(FuncionSerie, valor, iteracionesNecesarias, numpy.float64)
-    te = CalcularTEExperimental(FuncionSerie, valor, iteracionesNecesarias, numpy.float32, numpy.float64, 10)
-    print(f"cp: {cp}, te: {te}")
+    CalculoDeCPYTEPorBases(valor, iteracionesNecesarias, numpy.float64, numpy.float32, basesUsadas)    
 
 if __name__ == "__main__":
     Main()
